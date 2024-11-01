8d504b4172e951bee552b96516f73d67520d105d
0-Zz-ang <s1998319@gmail.com>
2024-11-01 14:56:25 +0900

    청크로 나누기

--- back/routes/api.js
+++ back/routes/api.js
@@ -6,7 +6,8 @@ const config = require('../config');
 
 
 
-// 로그인 라우터 
+
+// 로그인  
 router.post('/login', async (req, res) => {
   console.log('로그인 도착');
   const logDate = new Date().toISOString();
@@ -33,24 +34,28 @@ router.post('/login', async (req, res) => {
     if (checkResponse.data && checkResponse.data.data && checkResponse.data.data.length > 0) {
       const user = checkResponse.data.data[0];
       const storedPassword = user.cezUserPW;
-
+    
       if (password === storedPassword) {
         console.log(user.record_no, user.cezUserID, user.cezUserPW, logDate);
-
+    
         let loginSuccessDays = new Array(7).fill(0);
-
-        user._bp_lineitems.forEach(item => {
-          if (item.cezLogType === '로그인 성공') {
-            const prevLogDate = new Date(item.cezLogDate);
-            const dayIndex = prevLogDate.getDay(); 
-            loginSuccessDays[dayIndex] += 1;  
-          }
-        });
-
+        // 첫 로그인 
+        if (Array.isArray(user._bp_lineitems)) {
+          user._bp_lineitems.forEach(item => {
+            if (item.cezLogType === '로그인 성공') {
+              const prevLogDate = new Date(item.cezLogDate);
+              const dayIndex = prevLogDate.getDay(); 
+              loginSuccessDays[dayIndex] += 1;
+            }
+          });
+        }
+    
         const todayDayIndex = new Date().getDay();
-        loginSuccessDays[todayDayIndex] += 1; 
-
+        loginSuccessDays[todayDayIndex] += 1;
+    
         console.log(loginSuccessDays);
+    
+        // 로그인 성공 기록 업데이트
         await axios.put(`${config.unifierUrl}/ws/rest/service/v1/bp/record`, {
           options: {
             bpname: "member(Seo0)",
@@ -73,11 +78,12 @@ router.post('/login', async (req, res) => {
             'Content-Type': 'application/json'
           }
         });
-
-        const logTypes = user._bp_lineitems.map(item => item.cezLogType);
-        const loginSuccess = logTypes.filter(log => log === "로그인 성공").length;
+    
+        // 로그 데이터에서 성공과 실패 기록 계산
+        const logTypes = Array.isArray(user._bp_lineitems) ? user._bp_lineitems.map(item => item.cezLogType) : [];
+        const loginSuccess = logTypes.filter(log => log === "로그인 성공").length + 1;  
         const loginFail = logTypes.filter(log => log === "로그인 실패").length;
-
+    
         return res.status(200).json({
           message: '로그인 성공!',
           user: {
@@ -85,16 +91,17 @@ router.post('/login', async (req, res) => {
             name: user.cesusername,
             email: user.cezUserEmail,
             record_no: user.record_no,
-            CountLoginSuccess: loginSuccess,
+            CountLoginSuccess: loginSuccess,  
             CountLoginFail: loginFail,
             logDate: logDate,
-            loginSuccessDays: loginSuccessDays  
+            loginSuccessDays: loginSuccessDays
           }
         });
-
       } else {
         console.log("비밀번호 불일치");
 
+    
+        // 비밀번호 불일치 로그 저장
         await axios.put(`${config.unifierUrl}/ws/rest/service/v1/bp/record`, {
           options: {
             bpname: "member(Seo0)",
@@ -162,7 +169,7 @@ router.post('/signup', async (req, res) => {
         'Authorization' : `Bearer ${config.token}`,
         'Content-Type' : `application/json`
       }
-    });
+    }); 
 
     if (signUp.status === 200) {
       return res.status(200).json({ message: '회원가입 성공!' });
@@ -182,6 +189,8 @@ router.post('/signup', async (req, res) => {
     return res.status(500).json({ message: '유니파이어 API 호출 중 오류가 발생했습니다.', error: error.message });
   }
 });
+
+
 
 //중복검사 
 router.post('/checkId', async (req, res) => {
@@ -234,6 +243,8 @@ router.post('/checkId', async (req, res) => {
   }
 });
 
+
+
 // 로그아웃
 router.post('/logout', async (req, res) => {
   const {record_no} = req.body;
@@ -271,7 +282,6 @@ router.post('/logout', async (req, res) => {
 });
 
 
-// 비밀번호 변경 이메일 전송 
 router.post('/forgotpassword', async (req,res) => {
   const { id, email, mail_url } = req.body;
   console.log("비밀번호 변경 이메일 전송 라우터 도착")
@@ -331,6 +341,7 @@ router.post('/forgotpassword', async (req,res) => {
   }
 });
 
+
 // 비밀번호 업데이트 
 router.post('/updatePw', async(req,res) => {
   console.log('업데이트 라우터 도착');
@@ -380,15 +391,17 @@ router.get('/getProjects', async (req, res) => {
     const projectData = response.data.data.map(({ projectname, projectnumber }) => ({
       project_name: projectname,
       project_number: projectnumber,
-    }));
+    }))
     res.status(200).json(projectData);
+    console.log(projectData.project_number);
   } catch (error) {
     console.error('프로젝트 목록 조회 오류:', error);
     res.status(500).json({ message: '프로젝트 목록을 가져오는 중 오류가 발생했습니다.' });
   }
 });
 
-// BP 목록 조회 라우터
+
+// BP 목록 조회 
 router.get('/getBpList', async (req, res) => {
   const { project_number, project_name } = req.query;  // 쿼리 파라미터로 프로젝트 정보 수신
   console.log('Project Name:', project_name);
@@ -425,14 +438,16 @@ router.get('/getBpList', async (req, res) => {
   }
 });
 
+// 그리드 데이터 ⬇️
 
+// bp 데이터 조회 
 router.post('/getBpData', async (req, res) => {
   const { bpname,project_number }=req.body;
   try {
     let response;
     const body = {
         "bpname" : bpname,
-        "lineitem" : "no"
+        "lineitem" : "yes"
     }
     if(project_number==='0')
     {
@@ -452,7 +467,7 @@ router.post('/getBpData', async (req, res) => {
     }
 
     if (response.data) {
-      console.log(response.data);
+      console.log("bp data 목록 조회 : ", response.data);
       return res.status(200).json(response.data);
 
     } else {
@@ -464,5 +479,235 @@ router.post('/getBpData', async (req, res) => {
   }
 });
 
+const MAX_CHUNK_SIZE = 1 * 1024 * 1024; // 1MB
+
+function createChunksBySize(data) {
+  const chunks = [];
+  let currentChunk = [];
+  let currentSize = 0;
+
+  data.forEach((record) => {
+    // JSON.stringify(record)로 현재 레코드 크기 계산
+    const recordSize = Buffer.byteLength(JSON.stringify(record), 'utf8');
+
+    // 현재 레코드를 추가했을 때 청크가 MAX_CHUNK_SIZE를 넘지 않으면 추가
+    if (currentSize + recordSize <= MAX_CHUNK_SIZE) {
+      currentChunk.push(record);
+      currentSize += recordSize;
+    } else {
+      // 크기를 넘으면 현재 청크를 저장하고 새로운 청크 시작
+      chunks.push(currentChunk);
+      currentChunk = [record];
+      currentSize = recordSize;
+    }
+  });
+
+  // 마지막 청크가 남아있으면 추가
+  if (currentChunk.length > 0) {
+    chunks.push(currentChunk);
+  }
+
+  return chunks;
+}
+
+// Grid Data에 새로 입력된 데이터(로컬스토리지에 저장된 데이터)를 유니파이어에 전송 
+router.post('/sendData', async (req, res) => {
+  const { options, data } = req.body;
+  console.log("프로젝트 번호 : ", options.projectNumber);
+
+
+  const recordsWithoutRecordNo = data.filter(record => !record.record_no);
+  const recordsWithRecordNo = data.filter(record => record.record_no);
+
+  try {
+    // 신규 레코드 추가 - 청크로 나누기
+    const newRecordsChunks = createChunksBySize(recordsWithoutRecordNo);
+
+    const newRecordPromises = newRecordsChunks.map(chunk => {
+      const requestBody = {
+        options: {
+          bpname: options.bpName,
+        },
+        data: chunk.map(record => ({
+          ...record,
+          _bp_lineitems: record._bp_lineitems || []
+        }))
+      };
+
+      return axios.post( 
+        options.projectNumber === "0" ?
+        `${config.unifierUrl}/ws/rest/service/v1/bp/record` : `${config.unifierUrl}/ws/rest/service/v1/bp/record/${options.projectNumber}`,
+        requestBody,
+        {
+          headers: {
+            'Authorization': `Bearer ${config.token}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+    });
+
+    // 기존 레코드 수정 - 청크로 나누기
+    const updateRecordsChunks = createChunksBySize(recordsWithRecordNo);
+
+    const updateRecordPromises = updateRecordsChunks.map(chunk => {
+      const requestBody = {
+        options: {
+          bpname: options.bpName,
+          lineitemIdentifier: "LineAutoSeq"
+        },
+        data: chunk.map(record => ({
+          ...record,
+          _bp_lineitems: record._bp_lineitems || []
+        }))
+      };
+
+      return axios.put(
+        options.projectNumber === "0" ?
+        `${config.unifierUrl}/ws/rest/service/v1/bp/record` :
+        `${config.unifierUrl}/ws/rest/service/v1/bp/record/${options.projectNumber}`,
+        requestBody,
+        {
+          headers: {
+            'Authorization': `Bearer ${config.token}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+    });
+
+    // 모든 요청을 병렬로 처리
+    const responses = await Promise.all([...newRecordPromises, ...updateRecordPromises]);
+
+    // 응답 처리
+    const recordStatusList = responses.map(response => {
+      const status = response.data?.message[0]?._record_status || '알 수 없음';
+      console.log('Unifier 응답:', response.data);
+      return status;
+    });
+
+    // 결과 처리
+    const failedRecords = recordStatusList.filter(status => status !== 'success');
+    if (failedRecords.length === 0) {
+      console.log('모든 레코드 및 라인아이템 처리 성공');
+      res.status(200).json({ message: '모든 레코드와 라인아이템이 성공적으로 처리되었습니다.' });
+    } else {
+      console.error('일부 레코드 처리 중 오류 발생:', failedRecords);
+      res.status(400).json({ message: `일부 레코드 처리 중 오류 발생: ${failedRecords.join(', ')}` });
+    }
+
+  } catch (error) {
+    console.error('Unifier API 호출 중 오류 발생:', error);
+    res.status(500).json({ message: 'Unifier API 호출 중 오류가 발생했습니다.' });
+  }
+});
+
+
+
+
+
+// 라인아이템 삭제 
+router.post("/deleteLineItem", async (req, res) => {
+  const { bpName, projectNumber, targetItem, targetRecord } = req.body;
+
+  console.log("전달받은 데이터");
+  console.log(
+    "bpName:", bpName,
+    "projectNumber:", projectNumber,
+    "삭제할 번호:", targetItem,
+    "레코드:", targetRecord
+  );
+
+  try {
+    let response;
+    const body = {
+      options: {
+        bpname: bpName,
+        LineItemIdentifier: "LineAutoSeq",
+      },
+      data: [
+        {
+          _delete_bp_lineitems: targetItem,
+          record_no: targetRecord,
+        },
+      ],
+    };
+
+    // 프로젝트번호 0 == company workspace
+    if (projectNumber === '0') {
+      response = await axios.put(`${config.unifierUrl}/ws/rest/service/v1/bp/record`, body, {
+        headers: {
+          Authorization: `Bearer ${config.token}`,
+          'Content-Type': 'application/json',
+        },
+      });
+    } else {
+      response = await axios.put(`${config.unifierUrl}/ws/rest/service/v1/bp/record/${projectNumber}`, body, {
+        headers: {
+          Authorization: `Bearer ${config.token}`,
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    
+    if (response && response.data) {
+      res.status(200).send({ message: "LineItem deleted successfully", data: response.data });
+    } else {
+    
+      res.status(404).json({ message: 'BP 데이터를 찾을 수 없습니다.' });
+    }
+  } catch (error) {
+      console.error('BP 데이터 삭제 중 오류:', error.message);
+    
+    if (error.response) {
+      console.error('서버 응답 오류:', error.response.data);
+      return res.status(error.response.status || 500).json({
+        message: 'BP 데이터를 삭제하는 중 오류가 발생했습니다.',
+        error: error.response.data,
+      });
+    }
+
+    return res.status(500).json({
+      message: '서버 오류: BP 데이터를 삭제하는 중 문제가 발생했습니다.',
+      error: error.message,
+    });
+  }
+});
+
+
+router.post("/addLineItem", async (req, res) => {
+  const { bpName, projectNumber, recordNo, lineItemData } = req.body;
+
+  try {
+    const response = await axios.put(`${config.unifierUrl}/ws/rest/service/v1/bp/record${projectNumber === '0' ? '' : `/${projectNumber}`}`, {
+      options: {
+        bpname: bpName,
+        LineItemIdentifier: "LineAutoSeq",
+      },
+      data: [
+        {
+          record_no: recordNo,
+          _bp_lineitems: [
+            {
+              ...lineItemData,  // 추가할 라인아이템의 데이터
+              LineAutoSeq: "자동생성",  // 필요한 경우 자동생성된 시퀀스 값 사용
+            },
+          ],
+        },
+      ],
+    }, {
+      headers: {
+        Authorization: `Bearer ${config.token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    res.status(200).json({ message: "라인아이템 추가 성공", data: response.data });
+  } catch (error) {
+    console.error("라인아이템 추가 오류:", error.message);
+    res.status(500).json({ message: "라인아이템 추가 중 오류가 발생했습니다.", error: error.message });
+  }
+});
 
 module.exports = router;
--- back/server.js
+++ back/server.js
@@ -6,32 +6,27 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-
 app.use(cors());
 app.use(express.json());
-
+// app.use(express.json({ limit: '50mb' }));  // JSON 본문 크기 제한 설정
+// app.use(express.urlencoded({ limit: '50mb', extended: true }));  // URL-encoded 본문 크기 제한 설정
 
 app.use('/api', apiRoutes);
 console.log('API Routes가 로드되었습니다.');
 
-
 app.use(express.static(path.join(__dirname, 'build')));
-
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}/`);
 });
+
+
 
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
 });
-
-
-
-
